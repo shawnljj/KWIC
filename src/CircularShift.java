a08a64a79592679a641cd92eb1e630b6b36c6516
Shawn <shawnljj@gmail.com>
2017-08-30 10:26:33 +0800

    Add comment for how to do circular shift

--- src/CircularShift.java
+++ src/CircularShift.java
@@ -3,6 +3,8 @@ import java.util.ArrayList;
 public class CircularShift {
 
 	public ArrayList<Line> applyShift(ArrayList<MovieTitle> movieTitles, ArrayList<WordToIgnore> wordsToIgnore) {
+		//Circular shift all lines, then remove lines which begin with a wordToIgnore
+		
 		return null;
 		// TODO Auto-generated method stub
 		
